39d8741c5dc30f38f37bb102aca2288ce226da5d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-27 21:28:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    redirect_to '/sessions/new' if session[:name]
-    redirect_to(controller: 'application', action: 'hello')
+    if params[:name] && params[:name] != ""
+      session[:name] = params[:name]
+      redirect_to '/'
   end
 
   def destroy
     session.delete :name
-    redirect_to(controller: 'sessions', action: new)
+    redirect_to '/login'
   end
 end
