c6a65205d661a07663eda7fe085b541c1ff5a871
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-27 21:25:20 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    redirect_to(controller: 'sessions', action: 'new') if params[:name].empty? || params[:name] == nil
-    session[:name] = params[:name]
+    redirect_to '/sessions/new' if session[:name]
     redirect_to(controller: 'application', action: 'hello')
   end
 
