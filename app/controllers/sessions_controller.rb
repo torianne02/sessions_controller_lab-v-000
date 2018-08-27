4f0c584489e123dfa17738c550f966bad1a9d145
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-27 21:15:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:username] = params[:username]
+    redirect_to '/'
   end
 
   def destroy
+    session.delete :username
   end
 end
